6c0ee66ffaa35139b71eff820c8f2402ecdf15fb
leila <leilayaghoobi13700@gmail.com>
2020-06-18 12:15:43 -0700

    update

--- app/src/main/java/ir/siriusapps/moneysave/service/AppService.kt
+++ app/src/main/java/ir/siriusapps/moneysave/service/AppService.kt
@@ -1,52 +1,48 @@
 package ir.siriusapps.moneysave.service
 
-import android.R
 import android.app.*
 import android.content.Intent
 import android.os.Build
 import android.os.IBinder
-import android.util.Log
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 
 
 class AppService : Service() {
-
-    val CHANNEL_ID = "ForegroundServiceChannel"
     private var manager: NotificationManager? = null
+
+    companion object {
+        private const val chanelId = "ForegroundServiceChannel"
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
         TODO("Not yet implemented")
     }
 
     override fun onCreate() {
-
         super.onCreate()
         createNotificationChannel()
-        val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
+        val notification: Notification = NotificationCompat.Builder(this, chanelId)
             .setContentTitle("Foreground Service")
             .setContentText("MoneySave app is running")
-            .setSmallIcon(R.drawable.alert_light_frame)
             .build()
-        Log.i("TAG", "start")
         startForeground(1, notification)
-
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Toast.makeText(this, "start service", Toast.LENGTH_LONG).show()
         return super.onStartCommand(intent, flags, startId)
     }
 
     private fun createNotificationChannel() {
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificatoinChannel = NotificationChannel(
-                CHANNEL_ID,
+            val notificationChannel = NotificationChannel(
+                chanelId,
                 "App Notification",
-                NotificationManager.IMPORTANCE_LOW
+                NotificationManager.IMPORTANCE_HIGH
             )
             manager = getSystemService(NotificationManager::class.java)
-            manager?.createNotificationChannel(notificatoinChannel)
+            manager?.createNotificationChannel(notificationChannel)
         }
     }
-
 }
