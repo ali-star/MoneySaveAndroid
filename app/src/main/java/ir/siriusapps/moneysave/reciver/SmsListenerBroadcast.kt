8c4c930157222ef70632f053fb6389b4872f40f3
leila <leilayaghoobi1370@gmail.com>
2020-07-02 09:17:13 -0700

    update

--- app/src/main/java/ir/siriusapps/moneysave/reciver/SmsListenerBroadcast.kt
+++ app/src/main/java/ir/siriusapps/moneysave/reciver/SmsListenerBroadcast.kt
@@ -9,22 +9,32 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import java.lang.StringBuilder
 
-class SmsListenerBroadcast:BroadcastReceiver() {
+class SmsListenerBroadcast : BroadcastReceiver() {
+
+    val phoneNumberBanks = arrayListOf("9107324708", "9907473597")
+
     @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION == intent?.action){
+
+        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION == intent?.action) {
             val bundle = intent.extras
             if (bundle != null) {
                 try {
                     val smsMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
                     val messageBody = StringBuilder()
-                    var from: String? = null
-                    for (message in smsMessages) {
-                        if (from == null)
-                            from = message.originatingAddress
-                        messageBody.append(message.messageBody)
-                    }
-                    Toast.makeText(context, from + "\n" + messageBody.toString(), Toast.LENGTH_LONG).show()
+                    var senderNumber: String? = smsMessages[0].originatingAddress
+                    for (i in 0..phoneNumberBanks.size)
+                        if (senderNumber!!.contains(phoneNumberBanks[i])) {
+                            for (message in smsMessages)
+                                messageBody.append(message.messageBody)
+                            Toast.makeText(
+                                context,
+                                senderNumber + "\n" + messageBody.toString(),
+                                Toast.LENGTH_LONG
+                            )
+                                .show()
+                            break
+                        }
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
