5c7d9d4bf2a807ec25fec8845847cda1b8e4c54b
ali-star <obanstar1@gmail.com>
2020-08-07 16:47:25 +0430

    update

--- app/src/main/java/ir/siriusapps/moneysave/customView/GradientBlurLayout.kt
+++ app/src/main/java/ir/siriusapps/moneysave/customView/GradientBlurLayout.kt
@@ -2,11 +2,11 @@ package ir.siriusapps.moneysave.customView
 
 import android.content.Context
 import android.graphics.*
+import android.os.Build
 import android.util.AttributeSet
 import android.view.View
 import ir.siriusapps.moneysave.R
 import ir.siriusapps.moneysave.utils.Utils
-import okhttp3.internal.Util
 
 class GradientBlurLayout : View {
     private val paint = Paint()
@@ -29,8 +29,8 @@ class GradientBlurLayout : View {
         context,
         attributeSet
     ) {
-        initView()
         initAttr(attributeSet)
+        initView()
     }
 
     constructor(context: Context, attributeSet: AttributeSet?, defStyle: Int) : super(
@@ -39,19 +39,25 @@ class GradientBlurLayout : View {
         defStyle
     ) {
         initAttr(attributeSet)
+        initView()
     }
 
     private fun initAttr(attributeSet: AttributeSet?) {
-        context.obtainStyledAttributes(attributeSet, R.styleable.GradientBlurLayout).apply {
-            startColor = getColor(R.styleable.GradientBlurLayout_gbl_startColor, Color.BLACK)
-            endColor = getColor(R.styleable.GradientBlurLayout_gbl_endColor, Color.WHITE)
-            angel = getInt(R.styleable.GradientBlurLayout_gbl_angle, 45)
-            blurRadius = getFloat(R.styleable.GradientBlurLayout_gbl_blurRadius, Utils.dipToPix(8F))
-            radius = getFloat(R.styleable.GradientBlurLayout_gbl_radius, Utils.dipToPix(16F))
+        val typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.GradientBlurLayout)
+        try {
+            startColor = typedArray.getColor(R.styleable.GradientBlurLayout_gbl_startColor, Color.BLACK)
+            endColor = typedArray.getColor(R.styleable.GradientBlurLayout_gbl_endColor, Color.WHITE)
+            angel = typedArray.getInt(R.styleable.GradientBlurLayout_gbl_angle, 45)
+            blurRadius = typedArray.getFloat(R.styleable.GradientBlurLayout_gbl_blurRadius, Utils.dipToPix(8F))
+            radius = typedArray.getFloat(R.styleable.GradientBlurLayout_gbl_radius, Utils.dipToPix(16F))
+        } finally {
+            typedArray.recycle()
         }
     }
 
     private fun initView() {
+        if (Build.VERSION.SDK_INT < 29)
+            setLayerType(LAYER_TYPE_SOFTWARE, null)
         mMatrix.setRotate(angel.toFloat())
         backgroundRectBlur = RectF()
     }
