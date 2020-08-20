9a867800266aebfcf4c22188e487c16e0d023152
ali-star <obanstar1@gmail.com>
2020-08-20 16:15:33 +0430

    Fixed CreditCardView.

--- app/src/main/java/ir/siriusapps/moneysave/customView/CreditCardView.kt
+++ app/src/main/java/ir/siriusapps/moneysave/customView/CreditCardView.kt
@@ -1,7 +1,6 @@
 package ir.siriusapps.moneysave.customView
 
 import android.content.Context
-import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.drawable.Drawable
@@ -14,57 +13,57 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import ir.siriusapps.moneysave.R
 
-
-
 class CreditCardView : FrameLayout {
 
-    private var mWith: Float = 0F
-    private var mHeight: Float = 0F
-    private lateinit var paint: Paint
-    private lateinit var path: Path
     private lateinit var iconImageView: ImageView
     private lateinit var backgroundImageView: ImageView
     private lateinit var balanceTitleTextVew: TextView
     private lateinit var cardNumberTextView: TextView
     private lateinit var balanceTextView: TextView
     private var cardView: CardView? = null
+
     var cardColor: Int? = null
         set(value) {
             field = value
-            value?.let { cardView?.setBackgroundColor(it) }
+            value?.let { cardView?.setCardBackgroundColor(it) }
         }
+
     var backgroundImage: Drawable? = null
         set(value) {
             field = value
             value?.let { backgroundImageView.setImageDrawable(it) }
         }
+
     var iconImage: Drawable? = null
         set(value) {
             field = value
             value?.let { iconImageView.setImageDrawable(it) }
         }
+
     var cardNumber: String? = null
         set(value) {
             field = value
-            value?.let { cardNumberTextView.setText(it) }
+            value?.let { cardNumberTextView.text = it }
         }
-     var balanceTitle: String? = null
+
+    var balanceTitle: String? = null
         set(value) {
             field = value
-            value?.let { balanceTitleTextVew.setText(it) }
+            value?.let { balanceTitleTextVew.text = it }
         }
-     var balance: String? = null
+
+    var balance: String? = null
         set(value) {
             field = value
-            value?.let { balanceTextView.setText(it) }
+            value?.let { balanceTextView.text = it }
         }
 
     constructor(context: Context) : super(context) {
-        initView()
+        init()
     }
 
     constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
-        initView()
+        init()
         initAttribute(attributeSet)
     }
 
@@ -73,40 +72,38 @@ class CreditCardView : FrameLayout {
         attributeSet,
         defStyle
     ) {
-        initView()
+        init()
         initAttribute(attributeSet)
     }
 
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-    }
-
-    fun initView() {
-        if (Build.VERSION.SDK_INT < 29)
-            setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+    private fun init() {
         inflate(context, R.layout.cerdit_card_view, this)
-        /////////////////////////////////////
+
         iconImageView = findViewById(R.id.iconImageView)
         backgroundImageView = findViewById(R.id.backgroundImageView)
         balanceTitleTextVew = findViewById(R.id.balanceTitleTextView)
         balanceTextView = findViewById(R.id.balanceTextView)
         cardNumberTextView = findViewById(R.id.cardNumberTextView)
-        cardView = findViewById(R.id.cerditCardView)
-        //////////////////////////////////
+        cardView = findViewById(R.id.creditCardView)
+
         iconImage?.let {
             iconImageView.setImageDrawable(iconImage!!)
         }
+
         backgroundImage?.let {
             backgroundImageView.setImageDrawable(iconImage!!)
         }
+
         balanceTitle?.let {
-            balanceTitleTextVew.setText(it)
+            balanceTitleTextVew.text = it
         }
+
         balance?.let {
-            balanceTextView.setText(it)
+            balanceTextView.text = it
         }
+
         cardNumber?.let {
-            cardNumberTextView.setText(it)
+            cardNumberTextView.text = it
         }
     }
 
